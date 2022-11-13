53be791cde669008d3e0d38e1ca089da0e2e2a35
risingPhoenix7 <achinthyahebbar@gmail.com>
2022-11-13 21:37:57 +0530

    changed names

---
+++ MainApp.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class MainApp {
+    ArrayList<Student> userList;
+    ArrayList<Doctor> noticeBoard;
+    ArrayList<Medicine> medicineList;
+    ArrayList<Appointment> appointmentList;
+    ArrayList<Order> orderList;
+
+    public static void main(){
+        
+    }
+}
