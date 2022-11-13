f3c873015a2d46982039de1f1089bfcf9ad6bb43
Jash <jashnaikj@gmail.com>
2022-11-13 21:57:28 +0530

    Main name changed

--- Student.java
+++ Student.java
@@ -1,3 +1,45 @@
-public class Student {
-    
+class Student{
+    private String name;
+    private int bitsID;
+    private String mailID;
+    private int number;
+
+    Student(String name, int bitsID, String mailID, int number){
+        this.name = name;
+        this.bitsID = bitsID;
+        this. mailID = mailID;
+        this.number = number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBitsID(int bitsID) {
+        this.bitsID = bitsID;
+    }
+
+    public void setMailID(String mailID) {
+        this.mailID = mailID;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBitsID() {
+        return bitsID;
+    }
+
+    public String getMailID() {
+        return mailID;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
