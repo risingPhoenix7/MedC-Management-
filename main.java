3601dd5e1ea6f4b00a6d6c3ef98f42a39343f1e9
risingPhoenix7 <achinthyahebbar@gmail.com>
2022-11-13 21:26:28 +0530

    First commit

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class Main{
+    ArrayList<Student> userList;
+    ArrayList<Doctor>noticeBoard;
+
+    ArrayList<Medicine>medicineList;
+    ArrayList<Appointment>appointmentList;
+    ArrayList<Order>orderList;
+
+}
